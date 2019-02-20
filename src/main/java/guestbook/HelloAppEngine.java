a4d0a6508576cdb5077e9a153858c90a40389a3b
Rooshi Patidar <rooshipatidar@gmail.com>
2019-02-20 14:19:41 -0600

    Pushing guestbook code to App Engine to test

---
+++ src/main/java/guestbook/HelloAppEngine.java
@@ -0,0 +1,29 @@
+package guestbook;
+
+import java.io.IOException;
+
+import com.google.appengine.api.users.User;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+    User user
+
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
