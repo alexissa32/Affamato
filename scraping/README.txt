e0b995cfc14a110f4e8aecc24394d2103fad89ea
unknown <cameron.clark0821@utexas.edu>
2019-02-28 23:30:49 -0600

    spoonacular scraper can now handle requests for ingredient nurtritional information. readme updated to reflect that

--- scraping/README.txt
+++ scraping/README.txt
@@ -1,11 +1,25 @@
-In test.py:
-number = "" line determines the amount of recipes to fetch from spoonacular
-tags = "" to change the tags if looking for something in particular (not tested yet, may have to change how it is appended)
-filename = "" sets the filename to write to. PLEASE change if you don't want to overwrite a previous entry. Format: *.txt
-excelFileName = "" sets the excel filename to write to. PLEASE change if you don't want to overwrite a previous entry. Format: *.txt
+In spoonacularToJson.py:
+
 apiKey = "" is the api key for rapidAPI
+option = "--" is the option you are requesting. Currently supported are "recipes" and "food/ingredients"
+
+For Recipe requests:
+	make sure option is set to "recipes"
+	number = "" line determines the amount of recipes to fetch from spoonacular
+	tags = "" to change the tags if looking for something in particular (not tested yet, may have to change how it is appended)
+	random is boolean for if you are doing random recipe searches
+
+For Ingredient requests:
+	make sure option is set to "food/ingredients"
+	ingredientID is the spoonacular ID for the ingredient you are looking up
+	amount is the amount
+	unit is the unit of the amount
+	(amount and unit are required)
 
 Usage:
 Each api key gets a MAX of 50 requests and 500 results
 Each call will use NUMBER + 1 API calls. The first call will get all the recipe name and id's. Then a unique api call will find the ingredients for each recipe id
 ie NUMBER = 49 should be the max
+
+Recipe writes JSON files to a directory local/recipeJSONs
+Ingredient writes a JSON file to directory local/spoonacularIngredientJSONs
--- scraping/spoonacularToJson.py
+++ scraping/spoonacularToJson.py
@@ -3,12 +3,30 @@ Created on Feb 20, 2019
 
 @author: Cameron
 '''
+
+apiKey = "b738ce18bfmsh4e6a7c2d8aff98cp11356cjsn35dc321ea326"
+option = "recipes"
+'''
+options are:
+'recipes'
+'food/ingredients'
+'''
+
+#if doing recipes, modify the next 3
 number = "2";
 tags = "";
+random = True
+
+#if doing ingredients, modify the next 3
+ingredientID = "11215"
+amount = "10"
+unit = "gram"
+
+
+#end of modifiable
+
 #filename = "test.txt"
 excelFileName = "testWB.xlsx"
-apiKey = "db59991420msh5663a602972f8cbp111839jsn1f773f6d4213"
-
 
 import requests
 import json
@@ -20,43 +38,65 @@ excel = Workbook(excelFileName)
 excelWorksheet = excel.add_worksheet()
 row = 0
 column = 0
-baseurl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes"
+baseurl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
+baseOption = baseurl + option
+ingredient = False
 
-findIDurl = baseurl + "/random?"
+if random and option == "recipes":
+    findIDurl = baseOption + "/random?"
+elif option == "food/ingredients":
+    findIDurl = baseOption + "/" + ingredientID + "/information"
+    ingredient = True
+    if amount != "" and unit != "":
+        findIDurl = findIDurl + "?" + "amount=" + amount + "&" + "unit=" + unit
 
+if not ingredient:
+    if(number == ""):
+        number = "1"
+    number = "number=" + number 
+    if(tags != ""):
+        tags = "&tags=" + tags
 
-if(number == ""):
-    number = "1"
-number = "number=" + number 
-if(tags != ""):
-    tags = "&tags=" + tags
-
-findIDurl = findIDurl + number + tags
-#print(findIDurl)
-
-
+    findIDurl = findIDurl + number + tags
+    #print(findIDurl)
+    
+    
 recipes = []
-
+    
 responseList = requests.get(findIDurl,
     headers={
     "X-RapidAPI-Key": apiKey
   }
- )
-
+)
+    
 print(responseList)
-
+    
 recipeJson = responseList.json();
-
-
+    
+    
 print(recipeJson)
 cwd = os.getcwd()
-dumpDir = cwd + "\\recipeJSONs"
-recipeResults = recipeJson['recipes']
-i = 0
-for recipe in recipeResults:
-    newfile = str(i) + ".json"
-    with open(os.path.join(dumpDir,newfile), "w") as file1:
-        json.dump(recipe, file1)        
-    i = i + 1
+if ingredient:
+    dumpDir = cwd + "\\spoonacularIngredientJSONs"
+else:
+    dumpDir = cwd + "\\recipeJSONs"
     
- 
+if not ingredient:
+    recipeResults = recipeJson['recipes']
+    
+if not os.path.isdir(dumpDir):
+    os.mkdir(dumpDir)
+
+if not ingredient:
+    i = 0
+    for recipe in recipeResults:
+        newfile = str(i) + ".json"
+        with open(os.path.join(dumpDir,newfile), "w") as file1:
+            json.dump(recipe, file1)        
+        i = i + 1
+        
+else:
+    newfile = ingredientID + ".json"
+    with open(os.path.join(dumpDir,newfile), "w") as file1:
+            json.dump(recipeJson, file1) 
+     
