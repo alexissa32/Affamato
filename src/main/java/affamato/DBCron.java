c239c5a7648007afe5925a0a6a849a16f1491c6b
unknown <alex.issa32@utexas.edu>
2019-03-13 18:16:18 -0500

    Database automation working wowowowowow

--- src/main/java/affamato/DBCron.java
+++ src/main/java/affamato/DBCron.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +16,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONStringer;
 
+import com.google.appengine.api.datastore.Key;
 import com.googlecode.objectify.ObjectifyService;
 import affamato.Recipe;
 import affamato.Ingredient;
@@ -28,14 +31,30 @@ public class DBCron extends HttpServlet
        ObjectifyService.register(Ingredient.class);
     }
 	
+	//private static final Logger _logger = Logger.getLogger(DBCron.class.getName()); //Debug
+	
+	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
 		try
 		{
-			File dirIngredient = new File("./spoonacularIngredientJSONs/");
+			//String path = System.getProperty("java.class.path");
+			File dirIngredient = new File("./WEB-INF/classes/affamato/spoonacularIngredientJSONs/"); //("./src/main/java/spoonacularIngredientJSONs/");
+			File dirRecipe = new File("./WEB-INF/classes/affamato/recipeJSONs/");
+			
+			//String current = new File(".").getCanonicalPath(); //Debug
+			//_logger.info(current); //Debug
+			
 			File directoryListing[] = dirIngredient.listFiles();
-			if (directoryListing != null) 
+			File directoryListingTwo[] = dirRecipe.listFiles();
+			
+			if (directoryListing != null && directoryListingTwo != null) 
 			{
+				//Clear out the previous cron job's stuff
+				List<com.googlecode.objectify.Key<Ingredient>> keys = ofy().load().type(Ingredient.class).keys().list();
+				ofy().delete().keys(keys).now();
+				
+				//Load all the new (And technically old) recipes and ingredients
 				for (File f : directoryListing) 
 				{
 					BufferedReader br;
@@ -48,9 +67,32 @@ public class DBCron extends HttpServlet
 						line = br.readLine(); 
 					}
 					String jsonString = sb.toString();
+					//_logger.info(jsonString); //Debug
 				    Ingredient ing = new Ingredient(jsonString);
 				    ofy().save().entity(ing).now();
 				    ofy().load().entity(ing);
+				    br.close();			    
+				}
+				
+				List<com.googlecode.objectify.Key<Recipe>> keys2 = ofy().load().type(Recipe.class).keys().list();
+				ofy().delete().keys(keys2).now();
+				
+				for (File f : directoryListingTwo) 
+				{
+					BufferedReader br;
+					br = new BufferedReader(new FileReader(f));
+					StringBuilder sb = new StringBuilder();
+					String line = br.readLine(); 
+					while (line != null) 
+					{ 
+						sb.append(line);//.append("\n"); 
+						line = br.readLine(); 
+					}
+					String jsonString = sb.toString();
+					//_logger.info(jsonString); //Debug
+				    Recipe rec = new Recipe(jsonString);
+				    ofy().save().entity(rec).now();
+				    ofy().load().entity(rec);
 				    br.close();			    
 				}
 			}
@@ -59,5 +101,11 @@ public class DBCron extends HttpServlet
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		doGet(req,resp);
 	}
 }
