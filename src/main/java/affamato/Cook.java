8bb8cd5a9134e2db665f172ae3ed7974faadd100
unknown <alex.issa32@utexas.edu>
2019-04-19 17:17:51 -0500

    Stop pushing over my commits >:(

--- src/main/java/affamato/Cook.java
+++ src/main/java/affamato/Cook.java
@@ -47,6 +47,8 @@ public class Cook
     //UNTESTED METHOD correlated failures: removeFromPantry(), removeFromRecipeList()
     public void removeFromGroceryList(int pos) 
     {
+    	this.GroceryList.remove(pos);
+    	/**
     	JSONArray updated = new JSONArray();
     	try
     	{
@@ -62,7 +64,7 @@ public class Cook
     	{
     		e.printStackTrace();
     	}
-    	this.GroceryList = updated;
+    	this.GroceryList = updated; */
     }
     
     public void addToPantry(String ID) 
@@ -73,6 +75,8 @@ public class Cook
     //UNTESTED METHOD correlated failures: removeFromRecipeList(), removeFromGroceryList()
     public void removeFromPantry(int pos) 
     {
+    	this.Pantry.remove(pos);
+    	/**
     	JSONArray updated = new JSONArray();
     	try
     	{
@@ -88,7 +92,7 @@ public class Cook
     	{
     		e.printStackTrace();
     	}
-    	this.Pantry = updated;
+    	this.Pantry = updated; */
     }
     
     public void addToRecipeList(String RecipeName) 
@@ -99,6 +103,8 @@ public class Cook
     //UNTESTED METHOD correlated failures: removeFromPantry(), removeFromGroceryList()
     public void removeFromRecipeList(int pos) 
     {
+    	this.RecipeList.remove(pos);
+    	/**
     	JSONArray updated = new JSONArray();
     	try
     	{
@@ -114,7 +120,7 @@ public class Cook
     	{
     		e.printStackTrace();
     	}
-    	this.RecipeList = updated;
+    	this.RecipeList = updated; */
     }
     
     //UNTESTED METHOD correlated failures: getRecipeList(), getGroceryList()
