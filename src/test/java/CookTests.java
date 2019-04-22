4c11e2d63ef02599b8563e70609ef780537ea5db
justinhenry <justinhenry@utexas.edu>
2019-04-22 14:46:23 -0500

    empty Junit tests ready to be written

--- src/test/java/CookTests.java
+++ src/test/java/CookTests.java
@@ -3,13 +3,23 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.google.appengine.api.users.User;
+
+import affamato.Cook;
+
 @RunWith(Suite.class)
 @SuiteClasses({})
 public class CookTests {
 
+	String ingredientString1 = "";
+	String ingredientString2 = "";
+	String recipeString = "";
+
 	
 	@Test
 	public void addToGroceryListTest() {
+		User me = new User("me@gmail.com", "pizza.com");
+		Cook c = new Cook(me, "Cookholder"); 
 		
 	}
 	
@@ -33,5 +43,39 @@ public class CookTests {
 		
 	}
 	
+	@Test
+	public void addToRecipeListTest() {
+		
+	}
+	
+	@Test
+	public void removeFromRecipeListTest() {
+		
+	}
+	
+	@Test
+	public void PantrySearchResultsTest() {
+		
+	}
+	
+	@Test
+	public void GrocerySearchResultsTest() {
+		
+	}
+	
+	@Test
+	public void RecipeSearchResultsTest() {
+		
+	}
+	
+	@Test
+	public void getCookVoidTest() {
+		
+	}
+	
+	@Test
+	public void getUserTest() {
+		
+	}
 }
 
