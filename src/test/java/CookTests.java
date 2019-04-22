78e385d3c23549f615655ac752ef26534c7ce94c
justinhenry <justinhenry@utexas.edu>
2019-04-22 14:29:17 -0500

    half of the emtpy tests made

---
+++ src/test/java/CookTests.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class CookTests {
+
+	
+	@Test
+	public void addToGroceryListTest() {
+		
+	}
+	
+	@Test
+	public void removeGroceryListTest() {
+		
+	}
+	
+	@Test
+	public void removeFromGroceryListTest() {
+		
+	}
+	
+	@Test
+	public void addToPantryTest(){
+		
+	}
+	
+	@Test
+	public void removeFromPantryTest() {
+		
+	}
+	
+}
+
