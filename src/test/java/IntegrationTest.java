735aebecbe1a8382d757a6643cbc92f57712485d
Rooshi Patidar <rooshipatidar@gmail.com>
2019-04-22 17:53:17 -0500

    added selenium junit combination stuff

---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import static org.junit.Assert.assertEquals;
+
+ public class IntegrationTest {
+
+ 	@Test
+	public void testGoogle() throws InterruptedException {
+
+ 		System.setProperty("webdriver.chrome.driver", 
+				"C:\\chromedriver.exe");
+
+ 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+ 		Thread.sleep(5000);
+
+ 	}
+ }
