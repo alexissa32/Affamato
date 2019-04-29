06e4dac263e6199429188e26081b46a80d73d322
Jules <julialrebello@utexas.edu>
2019-04-29 00:25:15 -0500

    recipes stuff

--- src/main/java/affamato/FavoriteServlet.java
+++ src/main/java/affamato/FavoriteServlet.java
@@ -6,12 +6,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class FavoriteServlet extends HttpServlet{
+	
 //POST IS ADDING TO FAVORITES	
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     	
     	
         resp.sendRedirect("/recipesPage.jsp");
     }
+    
 //GET IS DISCOVER    
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     	
