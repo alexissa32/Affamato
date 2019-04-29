10eef7573f4b75d3abe8c4ef49f2defb79b8506f
Jules <julialrebello@utexas.edu>
2019-04-29 00:26:26 -0500

    favservlet

--- src/main/java/affamato/FavoriteServlet.java
+++ src/main/java/affamato/FavoriteServlet.java
@@ -9,14 +9,22 @@ public class FavoriteServlet extends HttpServlet{
 	
 //POST IS ADDING TO FAVORITES	
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    	
+	     String ingredient = req.getParameter("ingredient");	     
+	     String title = req.getParameter("discoverTitle");
+	     int prepMinutes = req.getParameter("prepMinutes");
+	     int cookMinutes = req.getParameter("cookMinutes");
+	     String instructions = req.getParameter("instructions");	     			
     	
         resp.sendRedirect("/recipesPage.jsp");
     }
     
 //GET IS DISCOVER    
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    	
+	     String ingredient = req.getParameter("ingredient");	     
+	     String title = req.getParameter("discoverTitle");
+	     int prepMinutes = req.getParameter("prepMinutes");
+	     int cookMinutes = req.getParameter("cookMinutes");
+	     String instructions = req.getParameter("instructions");	    	
     	
         resp.sendRedirect("/recipesPage.jsp");
     }
